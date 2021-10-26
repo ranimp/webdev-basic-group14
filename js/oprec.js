08e6c43bcc18d7259bb6734f1fce20399a8e0fb8
Rani Meliyana Putri <ranimeliyanaputri@gmail.com>
2021-10-26 20:44:16 +0800

    change logic jurusan & angkatan

--- js/oprec.js
+++ js/oprec.js
@@ -1,7 +1,5 @@
 const elInputNama = document.querySelector("#input-nama")
 const elInputEmail = document.querySelector("#input-email");
-const elInputJurusan = document.querySelector("#input-jurusan");
-const elInputAngkatan = document.querySelector("#input-angkatan");
 const elInputTelepon = document.querySelector("#input-telepon");
 const elInputAlamat = document.querySelector("#input-alamat");
 const elInputAlasan = document.querySelector("#input-alasan");
@@ -14,8 +12,6 @@ const angkatanValidator = /^20+[0-9]*$/;
 
 const elHelpNama = document.querySelector("#nama-help")
 const elHelpEmail = document.querySelector("#email-help");
-const elHelpJurusan = document.querySelector("#jurusan-help");
-const elHelpAngkatan = document.querySelector("#angkatan-help");
 const elHelpTelepon = document.querySelector("#telepon-help");
 const elHelpAlamat = document.querySelector("#alamat-help");
 const elHelpAlasan = document.querySelector("#alasan-help");
@@ -29,10 +25,6 @@ const elBtnSubmit = document.querySelector("#btn-submit").addEventListener("clic
 
   !elInputEmail.value.match(emailValidator) ? elHelpEmail.innerHTML = '<span style="color:red;">Gunakan email dari UMRAH</span>' : elHelpEmail.innerHTML = "";
 
-  elInputJurusan.value.length === 0 ? elHelpJurusan.innerHTML = '<span style="color:red;">Jurusan wajib diisi!</span>': elHelpJurusan.innerHTML = "";
-
-  elInputAngkatan.value.length != 4 || !elInputAngkatan.value.match(angkatanValidator) ? elHelpAngkatan.innerHTML = '<span style="color:red;">Masukkan tahun angkatan yang valid</span>' : elHelpAngkatan.innerHTML = "";
-
   elInputTelepon.value.length < 10 ? elHelpTelepon.innerHTML = '<span style="color:red;">Masukkan nomor telepon yang valid</span>' : elHelpTelepon.innerHTML = "";
 
   elInputAlamat.value.length === 0 ? elHelpAlamat.innerHTML = '<span style="color:red;">Alamat wajib diisi!</span>' : elHelpAlamat.innerHTML = "";
@@ -41,7 +33,7 @@ const elBtnSubmit = document.querySelector("#btn-submit").addEventListener("clic
 
   elInputCheck.checked ? '' : alert ("silahkan centang data");
 
-  if (elInputNama.value.match(namaValidator) && elInputNama.value.length > 0 && elInputEmail.value.match(emailValidator) && elInputJurusan.value.length > 0 && elInputAngkatan.value.length === 4 && elInputTelepon.value.length >= 10 && elInputAlamat.value.length > 0 && elInputAlasan.value.length > 0 && elInputCheck.checked === true){
+  if (elInputNama.value.match(namaValidator) && elInputNama.value.length > 0 && elInputEmail.value.match(emailValidator) && elInputTelepon.value.length >= 10 && elInputAlamat.value.length > 0 && elInputAlasan.value.length > 0 && elInputCheck.checked === true){
     location.href = "/done-oprec.html";
   }
   
