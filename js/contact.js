ee4a19dee30bb42f1fd124a0d92378a3ca32ee81
Rani Meliyana Putri <ranimeliyanaputri@gmail.com>
2021-10-26 13:50:14 +0800

    fixing contact logic

--- js/contact.js
+++ js/contact.js
@@ -1,15 +1,19 @@
 const elInputEmail = document.querySelector("#input-email");
 const elInputMessage = document.querySelector("#input-message");
 const elForm = document.querySelector("#form");
+const elEmailHelp = document.querySelector("#email-help");
+const elMessageHelp = document.querySelector("#message-help");
+
 const emailValidator = /[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]*/;
 
 const elBtnSubmit = document.querySelector("#btn-submit").addEventListener("click", (event) => {
   event.preventDefault()
-  if (!elInputEmail.value.match(emailValidator)){
-    alert('tolong masukkan email yang valid')
-  } else if (elInputMessage.value.length < 20){
-    alert('tolong masukkan pesan lebih dari 20 karakter')
-  } else {
+  
+  !elInputEmail.value.match(emailValidator) ? elEmailHelp.innerHTML = '<span style="color:red">Masukkan email yang valid!</span>' : elEmailHelp.innerHTML = ''
+  
+  elInputMessage.value.length < 20 ? elMessageHelp.innerHTML = '<span style="color:red">Pesan harus lebih dari 20 karakter!</span>' : elMessageHelp.innerHTML = ''
+  
+  if (elInputEmail.value.match(emailValidator) && elInputMessage.value.length >= 20){
     location.href = "/done-kontak.html";
   }
 })
